0d789cff92c225d50f715e054be9599e12b17db3
maridoh13 <maridoh@gmail.com>
2020-02-12 03:16:46 +0000

    update

---
+++ asyncBreeds.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+
+const breedDetailsFromFile = function(breed, cb) {
+  // console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    // console.log("In readFile's Callback: it has the data.");
+    if (!error) {
+      cb(data);
+    } else {
+      cb(undefined);
+    }
+  });
+
+};
+
+
+module.exports = breedDetailsFromFile;
